2691bcd882edac08d02ac8122b52c901f0523e52
ABISHA20561 <1abidass2056@gmail.com>
2026-04-26 22:32:22 +0530

    Update API URL to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:4000/api" });
-
+const API = axios.create({ baseURL: "https://recipe-app-backend-n59b.onrender.com/api" });
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
 export default API;
